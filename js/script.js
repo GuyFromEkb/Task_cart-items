4794f4d08e5ceed971ea76dbef5a719eda84e9f1
Sergey <graygmai1@gmail.com>
2022-07-05 14:05:30 +0500

    числовой индефикатор товара

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    document.addEventListener('click', (e) => {
+        const target = e.target;
 
+        if (target.classList.contains('items__control')) {
+
+            const atr = target.getAttribute(('data-action'));
+            const counter = target.closest('.counter-wrapper').querySelector('[data-counter]');
+
+
+            if (atr == "plus") { counter.innerText = ++counter.innerText; }
+
+            if (atr == "minus") {
+                if (counter.innerText > 1) { counter.innerText = --counter.innerText; }
+            }
+
+
+
+            // console.log(atr, counter);
+        }
+
+
+
+
+
+
+
+    });
 
 });
